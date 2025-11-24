e85ba010718e6d0e569af4b94e5f64dcfa1a06b4
adammKass <adam.kascak@protonmail.com>
2025-11-24 02:16:01 +0100

    -disabled additional snap effect for optimization

--- src/App.jsx
+++ src/App.jsx
@@ -3,93 +3,32 @@ import { heroSlides } from "./constants";
 import Section from "./components/Section";
 import { ReactLenis } from "lenis/react";
 import { useEffect, useRef } from "react";
-import { syncScroll } from "./utils/scrollStore";
+import { frame, cancelFrame } from "framer-motion";
 
 function App() {
-  const sectionRefs = useRef([]);
-  sectionRefs.current = [];
-
-  // Implementing lenis for smooth scrolling
-  const lenisRef = useRef();
+  const lenisRef = useRef(null);
 
   useEffect(() => {
-    function update(time) {
-      const lenis = lenisRef.current?.lenis;
-
-      if (lenis) {
-        lenis.raf(time);
-      }
-
-      requestAnimationFrame(update);
+    function update(data) {
+      const time = data.timestamp;
+      lenisRef.current?.lenis?.raf(time);
     }
 
-    requestAnimationFrame(update);
+    frame.update(update, true);
+
+    return () => cancelFrame(update);
   }, []);
 
-  let isScrolling;
-  const snapDuration = 1.2;
-
-  function snapToClosestSection() {
-    const lenis = lenisRef.current?.lenis;
-    if (!lenis) return;
-
-    const scrollPos = lenis.scroll;
-    const sections = sectionRefs.current;
-
-    // Find closest section top
-    let closest = sections[0];
-    let minDist = Math.abs(sections[0].offsetTop - scrollPos);
-
-    for (let s of sections) {
-      const dist = Math.abs(s.offsetTop - scrollPos);
-      if (dist < minDist) {
-        minDist = dist;
-        closest = s;
-      }
-    }
-
-    lenis.scrollTo(closest, {
-      duration: 1.0,
-      easing: (t) => 1 - Math.pow(1 - t, 3),
-    });
-  }
-  // ⭐ Add scroll sync here
-  useEffect(() => {
-    const lenis = lenisRef.current?.lenis;
-    if (!lenis) return;
-
-    const handler = (e) => {
-      clearTimeout(isScrolling);
-
-      // When user stops scrolling for 150ms → snap
-      isScrolling = setTimeout(() => {
-        snapToClosestSection();
-      }, 150);
-    };
-
-    lenis.on("scroll", handler);
-
-    return () => {
-      lenis.off("scroll", handler);
-    };
-  }, []);
   return (
     <>
-      <ReactLenis root ref={lenisRef} />
+      <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />
       <Navbar></Navbar>
-      {
-        // Snapping Container - not working beacuse of lenis
-        /* <div className="h-dvh overflow-y-scroll snap-y snap-mandatory"> */
-      }
 
       {/* <Hero /> */}
 
       {/* Passing text from constants.js to Section.jsx */}
       {heroSlides.map((slide, index) => (
         <Section
-          ref={(el) => {
-            if (el) sectionRefs.current.push(el);
-          }}
           key={index}
           name={slide.name}
           image={slide.image}
@@ -97,7 +36,6 @@ function App() {
           subheading={slide.subheading}
         />
       ))}
-      {/* </div> */}
     </>
   );
 }
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -5,68 +5,54 @@ import CTAButton from "./CTAButton";
 
 // Section parallax effect, seperate image and text containers for parallax effect
 
-const Section = forwardRef(
-  ({ name, image, imageMobile, subheading }, forwardedRef) => {
-    // Some forwarding ref black magic for section snapping with lenis, ont reallz understand it
+const Section = ({ name, image, imageMobile, subheading }) => {
+  // Some forwarding ref black magic for section snapping with lenis, ont reallz understand it
 
-    const localRef = useRef(null);
+  const localRef = useRef(null);
 
-    // Assign both local and forwarded ref
-    const setRefs = (el) => {
-      localRef.current = el;
+  // Assign both local and forwarded ref
 
-      if (typeof forwardedRef === "function") {
-        forwardedRef(el); // callback ref (your App.jsx)
-      } else if (forwardedRef) {
-        forwardedRef.current = el; // object ref
-      }
-    };
+  // Trigger on Scroll
+  const { scrollYProgress } = useScroll({
+    target: localRef,
+    offset: ["start end", "end start"],
+  });
 
-    // Trigger on Scroll
-    const { scrollYProgress } = useScroll({
-      target: localRef,
-      offset: ["start end", "end start"],
-    });
+  // Different values for text and image for parallax effect
+  const yBg = useTransform(scrollYProgress, [0, 1], ["-40%", "40%"]);
+  const yText = useTransform(scrollYProgress, [0, 1], ["-200%", "200%"]);
 
-    // Different values for text and image for parallax effect
-    const yBg = useTransform(scrollYProgress, [0, 1], ["-40%", "40%"]);
-    const yText = useTransform(scrollYProgress, [0, 1], ["-200%", "200%"]);
-
-    return (
-      // Overflow hidden to prevent overflow from image
-      <section
-        ref={setRefs}
-        className="relative min-h-lvh flex flex-col items-center md:items-start justify-end overflow-hidden"
+  return (
+    // Overflow hidden to prevent overflow from image
+    <section
+      ref={localRef}
+      className="relative min-h-lvh flex flex-col items-center md:items-start justify-end overflow-hidden"
+    >
+      {/* Image container, height slightly larger for parallax effect - image comes in faster, if bigger height not included -> white area */}
+      <motion.div className="absolute w-full h-[120%] -z-10" style={{ y: yBg }}>
+        {/* Dark overlay for better text visibility */}
+        <div className="absolute inset-0 bg-black/20"></div>
+        <picture className="w-full h-full object-cover">
+          <source media="(max-width: 768px)" srcSet={imageMobile} />
+          <img
+            className="w-full h-full object-cover object-top"
+            src={image}
+            alt={name}
+          />
+        </picture>
+      </motion.div>
+      {/* Text container */}
+      <motion.div
+        style={{ y: yText }}
+        className="content text-white text-center md:text-left flex flex-col gap-4 mb-[10vh]"
       >
-        {/* Image container, height slightly larger for parallax effect - image comes in faster, if bigger height not included -> white area */}
-        <motion.div
-          className="absolute w-full h-[120%] -z-10"
-          style={{ y: yBg }}
-        >
-          {/* Dark overlay for better text visibility */}
-          <div className="absolute inset-0 bg-black/20"></div>
-          <picture className="w-full h-full object-cover">
-            <source media="(max-width: 768px)" srcSet={imageMobile} />
-            <img
-              className="w-full h-full object-cover object-top"
-              src={image}
-              alt={name}
-            />
-          </picture>
-        </motion.div>
-        {/* Text container */}
-        <motion.div
-          style={{ y: yText }}
-          className="content text-white text-center md:text-left flex flex-col gap-4 mb-[10vh]"
-        >
-          <h2 className="font-serif font-bold text-2xl lg:text-6xl uppercase">
-            {name}
-          </h2>
-          <p className="text-sm sm:text-base max-w-prose">{subheading}</p>
-          <CTAButton path="#" text="Learn More" />
-        </motion.div>
-      </section>
-    );
-  }
-);
+        <h2 className="font-serif font-bold text-2xl lg:text-6xl uppercase">
+          {name}
+        </h2>
+        <p className="text-sm sm:text-base max-w-prose">{subheading}</p>
+        <CTAButton path="#" text="Learn More" />
+      </motion.div>
+    </section>
+  );
+};
 export default Section;
